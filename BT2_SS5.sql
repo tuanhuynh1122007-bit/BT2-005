86390b343145ba4eb8975e7a8ba1571661b74be2
tuanhuynh1122007-bit <tuanhuynh1122007@gmail.com>
2026-05-08 14:32:07 +0700

    Create and use ss05_bt02 database for queries
    Create database and use it for querying restaurants.

--- BT2_SS5.sql
+++ BT2_SS5.sql
@@ -1,4 +1,5 @@
-SELECT restaurant_name,created_at
-FROM Restaurants
-ORDER BY created_at DESC
-LIMIT 5;
+CREATE DATABASE ss05_bt02;
+USE ss05_bt02;
+-- phaỉ sắp xếp thứ tự tạo trước rồi mới limit như vậy mới lấy ra những quán mới trên sàn đc
+SELECT restaurant_name, created_at
+FROM Restaurants ORDER BY created_at DESC LIMIT 5;
